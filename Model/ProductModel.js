84dc96c3105b26c967c48d0528098146206f539e
Tanvir <ibnmizan7878@gmail.com>
2022-02-06 18:38:08 +0600

    updated product model

--- Model/ProductModel.js
+++ Model/ProductModel.js
@@ -7,6 +7,22 @@ const productSchema = mongoose.Schema({
   required: true
  },
  details: String,
+ lifePoint : {
+  type: Number,
+  default: 0,
+ },
+ quantity: {
+  type: Number,
+  default: 1,
+ },
+ available: {
+  type: Boolean,
+  default: true,
+ },
+ pricePerDay: {
+  type: Number,
+  required: true,
+ }
 });
 
 export default mongoose.model("products", productSchema);
