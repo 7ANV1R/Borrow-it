487383fc4297e8811fbaa7b726966b7741b62a83
7anv1r <ibnmizan7878@gmail.com>
2022-02-07 12:46:25 +0600

    added patch and delete method

--- Router/ProductAPI.js
+++ Router/ProductAPI.js
@@ -20,18 +20,42 @@ router.get("/", (req, res) => {
   if (err) {
    res.status(500).send(err);
   } else {
-   res.status(201).send(data);
+   res.status(200).send(data);
   }
  });
 });
 
 router.get("/:id", (req, res) => {
  const id = req.params.id
- Product.findById(id).exec( (err, data) => {
+ Product.findById(id).exec((err, data) => {
   if (err) {
    res.status(500).send(err);
   } else {
-   res.status(201).send(data);
+   res.status(200).send(data);
+  }
+ });
+});
+
+router.patch("/:id", (req, res) => {
+ const prod = req.body;
+ const id = req.params.id
+ Product.findByIdAndUpdate(id, prod,{new: true}).then((data, err) => {
+  if (err) {
+   res.status(406).send(err);
+  } else {
+   res.status(202).send(data);
+  }
+ })
+});
+
+
+router.delete("/:id", (req, res) => {
+ const id = req.params.id
+ Product.findByIdAndDelete(id).exec((err, data) => {
+  if (err) {
+   res.status(406).send(err);
+  } else {
+   res.status(202).send(`This ${id}'s data was deleted\n`+data);
   }
  });
 });
